83c443100b26842782bfaed24191c284e79adb29
FrancoAcevedoE <frankling1521@gmail.com>
2023-10-10 23:17:16 -0300

    Agregar archivos de proyecto.

---
+++ Web/Models/CineUTN.cs
@@ -0,0 +1,16 @@
+﻿namespace Web.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+public class CineUTN : DbContext
+{
+    public CineUTN(DbContextOptions<CineUTN> options) : base(options)
+    {
+
+    }
+    public DbSet<CineUTN> CineUTNs { get; set; }
+}
+
+
